e74706911640df3b2b6a4e089fd109858a60a53b
Mauricio Acencio <mgaf-muat@hotmail.com>
2017-04-25 18:46:44 -0300

    Arreglado código para utilizar métodos de valores (getters)

--- src/main/java/dci/logica/EscalaDeGrises.java
+++ src/main/java/dci/logica/EscalaDeGrises.java
@@ -6,7 +6,6 @@
 package dci.logica;
 
 import java.awt.Color;
-import java.awt.image.BufferedImage;
 
 /**
  *
@@ -16,13 +15,13 @@ public class EscalaDeGrises implements Filtro {
 
     @Override
     public Imagen aplicarFiltro(Imagen im) {
-        for (int x = 0; x < im.ancho; x++) {
-            for (int y = 0; y < im.alto; y++) {
-                int r = im.bitmap[x][y].getRed();
-                int g = im.bitmap[x][y].getGreen();
-                int b = im.bitmap[x][y].getBlue();
+        for (int x = 0; x < im.getAncho(); x++) {
+            for (int y = 0; y < im.getAlto(); y++) {
+                int r = im.getBitmap()[x][y].getRed();
+                int g = im.getBitmap()[x][y].getGreen();
+                int b = im.getBitmap()[x][y].getBlue();
                 int gris = Math.round((float) (r + g + b) / 3);
-                im.bitmap[x][y] = new Color(gris, gris, gris);
+                im.getBitmap()[x][y] = new Color(gris, gris, gris);
             }
         }
         return im;
