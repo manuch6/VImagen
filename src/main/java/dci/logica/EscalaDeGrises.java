e829c7f725a2c83b441ccd3a4194b2988b74991d
Mauricio Acencio <mgaf-muat@hotmail.com>
2017-04-25 16:57:51 -0300

    código primitivo del filtro escala de grises

--- src/main/java/dci/logica/EscalaDeGrises.java
+++ src/main/java/dci/logica/EscalaDeGrises.java
@@ -9,6 +9,19 @@ package dci.logica;
  *
  * @author dci
  */
-public class EscalaDeGrises {
-    
+public class EscalaDeGrises implements Filtro {
+
+    @Override
+    public Imagen aplicarFiltro(Imagen im) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                int r = 1;
+                int g = 2;
+                int b = 3;
+                int gris = Math.round((float) (r + g + b) / 3);
+            }
+        }
+        return new Imagen();
+    }
+
 }
