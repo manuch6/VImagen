20552bb924401f005c3978b65a4cc58f8957e9e7
CeeIciUfro <ceeiciufro@gmail.com>
2017-04-25 17:45:17 -0300

    cambios a filtro Negativo

--- src/main/java/dci/logica/Negativo.java
+++ src/main/java/dci/logica/Negativo.java
@@ -19,17 +19,17 @@ public class Negativo implements Filtro{
     @Override
     public Imagen aplicarFiltro(Imagen im) {
         int maxRGB = 255;
-//        for(int i=0; i< im.largo;i++){
-//            for(int j=0; j< im.ancho; j++){
-//                int rojo = im.imagen()[i][j].getRed();
-//                int verde = im.imagen()[i][j].getGreen();
-//                int azul = im.imagen()[i][j].getBlue();
-//                int rojoNeg = Math.abs(maxRGB-rojo);
-//                int verdeNeg = Math.abs(maxRGB-verde);
-//                int azulNeg = Math.abs(maxRGB-azul);
-//                im.imagen()[i][j] = new Color(rojoNeg,verdeNeg,azulNeg);
-//            }
-//        }
+        for(int i=0; i< im.getAlto();i++){
+            for(int j=0; j< im.getAncho(); j++){
+                int rojo = im.getBitmap()[i][j].getRed();
+                int verde = im.getBitmap()[i][j].getGreen();
+                int azul = im.getBitmap()[i][j].getBlue();
+                int rojoNeg = Math.abs(maxRGB-rojo);
+                int verdeNeg = Math.abs(maxRGB-verde);
+                int azulNeg = Math.abs(maxRGB-azul);
+                im.getBitmap()[i][j] = new Color(rojoNeg,verdeNeg,azulNeg);
+            }
+        }
         return im;
     }
     
